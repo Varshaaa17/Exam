9a4cc9f88c086efb9c32907eb9a87f942e62ebb5
Varshaaa17 <163492914+Varshaaa17@users.noreply.github.com>
2025-10-27 15:06:29 +0530

    Refactor form submission to use new fields

--- App.py
+++ App.py
@@ -5,10 +5,10 @@ def index():
     return render_template("form.html")
 @app.route('/submit',methods=['POST'])
 def submit():
-    username=request.form.get('username')
+    name=request.form.get('name')
     email=request.form.get('email')
     year=request.form.get('year')
-    phone=request.form.get('phone')
-    return render_template('result.html',name=username,email=email,year=year,phone=phone)
+    rollno=request.form.get('rollno')
+    return render_template('result.html',name=name,email=email,year=year,phone=phone)
 if __name__ == "_main_":
     app.run(host='0.0.0.0',port="5000",debug=True)
